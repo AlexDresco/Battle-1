87ce9c334b697680e2baced9ddad06baf27cb9db
Amy Denny-Guerin <a.dennyguerin@gmail.com>
2018-09-20 16:34:46 +0100

    Last day - up to SRP

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,14 +1,26 @@
 require 'game'
 
 describe Game do
-
-  let(:amy) { Player.new("Amy") }
-  let(:alex) { Player.new("Alex") }
+  subject { Game.new(player1,player2) }
+  let(:player1) { Player.new("Amy") }
+  let(:player2) { Player.new("Alex") }
 
   describe '#attack' do
     it 'should attack Player 2' do
-      expect(alex).to receive(:is_attacked)
-      subject.attack(alex)
+      expect(player2).to receive(:is_attacked)
+      subject.attack(player2)
+    end
+  end
+
+  describe "#player1" do
+    it 'returns players 1 name' do
+      expect(subject.player1).to eq player1
+    end
+  end
+
+  describe "#player2" do
+    it 'returns players 2 name' do
+      expect(subject.player2).to eq player2
     end
   end
 
